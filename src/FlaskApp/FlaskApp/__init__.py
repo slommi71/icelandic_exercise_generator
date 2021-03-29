b2dc5d4dfcfebd54ea65307b252b959755e913ae
Holger Slomka <holger.slomka@f-i-ts.de>
2021-03-29 19:28:08 +0200

    Ausgabe Geschlecht

--- src/FlaskApp/FlaskApp/__init__.py
+++ src/FlaskApp/FlaskApp/__init__.py
@@ -1,38 +1,26 @@
-# Add the site-packages of the chosen virtualenv to work with
 import random
 import yaml
 import site
+#import logging
+
+# Add the site-packages of the chosen virtualenv to work with
 site.addsitedir('/var/www/FlaskApp/FlaskApp/venv/lib/python3.8/site-packages')
+from flask_caching import Cache
 from flask import Flask, render_template
-from common import cache
 
 
 app = Flask(__name__)
-cache.init_app(app=app, config={"CACHE_TYPE": "filesystem",'CACHE_DIR': Path('/tmp')})
 
-
-# class wordbook:
-#     def __init__(self, words):
-#         self.words = words
-#         self.wordidx, self.word = self.get_next_word()
-
-#     def get_next_word(self):
-#         idx = random.randint(0, len(self.words)-1)
-#         return idx, self.words[idx]
-
-#     def get_word(self):
-#         return self.word
-
-#     def get_word_by_index(self, idx):
-#         return self.words[idx]
+# Instantiate the cache
+cache = Cache()
+cache.init_app(app=app, config={"CACHE_TYPE": "SimpleCache"})
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    #w = wb.get_word()
     idx, w = get_word(get_ordabok())
     cache.set("wordidx", idx)
-    # return "Lææa islensku"
+    #return render_template('index.html', wort=str(idx))
     return render_template('index.html', wort=w['de'])
 
 
@@ -45,16 +33,19 @@ def get_word(words):
     idx = random.randint(0, len(words)-1)
     return idx, words[idx]
 
+
 def get_word_by_index(words, idx):
-    idx = random.randint(0, len(words)-1)
-    return idx, words[idx]
+    return words[idx]
+
 
 @app.route('/lausn', methods=['POST'])
 def show_solution():
-    w = get_word_by_index(get_ordabok(), cache.get("wordidx"))
+    idx = cache.get("wordidx")
+    #logging.debug("indx {0}".format(idx))
+    w = get_word_by_index(get_ordabok(), int(idx))
     return render_template('lausn.html', wort=w['is'])
+    #return render_template('lausn.html', wort="str(idx)")
 
 
 if __name__ == "__main__":
-    # wb = wordbook(get_ordabok())
-    app.run()
+    app.run(debug=True)
