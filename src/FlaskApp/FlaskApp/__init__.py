477030c995604579408d89951980c1d1f47cf0af
Holger Slomka <holger.slomka@f-i-ts.de>
2021-03-29 18:43:20 +0200

    using cache to store word index

--- src/FlaskApp/FlaskApp/__init__.py
+++ src/FlaskApp/FlaskApp/__init__.py
@@ -4,30 +4,34 @@ import yaml
 import site
 site.addsitedir('/var/www/FlaskApp/FlaskApp/venv/lib/python3.8/site-packages')
 from flask import Flask, render_template
+from common import cache
+
 
 app = Flask(__name__)
+cache.init_app(app=app, config={"CACHE_TYPE": "filesystem",'CACHE_DIR': Path('/tmp')})
 
 
-class wordbook:
-    def __init__(self, words):
-        self.words = words
-        self.wordidx, self.word = self.get_next_word()
+# class wordbook:
+#     def __init__(self, words):
+#         self.words = words
+#         self.wordidx, self.word = self.get_next_word()
 
-    def get_next_word(self):
-        idx = random.randint(0, len(self.words)-1)
-        return idx, self.words[idx]
+#     def get_next_word(self):
+#         idx = random.randint(0, len(self.words)-1)
+#         return idx, self.words[idx]
 
-    def get_word(self):
-        return self.word
+#     def get_word(self):
+#         return self.word
 
-    def get_word_by_index(self, idx):
-        return self.words[idx]
+#     def get_word_by_index(self, idx):
+#         return self.words[idx]
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    # w = get_word(get_ordabok())
-    w = wb.get_word()
+    #w = wb.get_word()
+    idx, w = get_word(get_ordabok())
+    cache.set("wordidx", idx)
     # return "Lææa islensku"
     return render_template('index.html', wort=w['de'])
 
@@ -39,15 +43,18 @@ def get_ordabok():
 
 def get_word(words):
     idx = random.randint(0, len(words)-1)
-    return words[idx]
+    return idx, words[idx]
 
+def get_word_by_index(words, idx):
+    idx = random.randint(0, len(words)-1)
+    return idx, words[idx]
 
 @app.route('/lausn', methods=['POST'])
 def show_solution():
-    w = get_word(get_ordabok())
+    w = get_word_by_index(get_ordabok(), cache.get("wordidx"))
     return render_template('lausn.html', wort=w['is'])
 
 
 if __name__ == "__main__":
-    wb = wordbook(get_ordabok())
+    # wb = wordbook(get_ordabok())
     app.run()
