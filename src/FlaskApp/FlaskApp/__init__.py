77e77bc80ff8d034b1f84ca511eaefcb73a46a4b
Holger Slomka <holger.slomka@f-i-ts.de>
2021-03-30 15:23:19 +0200

    use SimpleCache

--- src/FlaskApp/FlaskApp/__init__.py
+++ src/FlaskApp/FlaskApp/__init__.py
@@ -15,7 +15,7 @@ logging.basicConfig(stream=sys.stderr)
 
 config = {
     "DEBUG": True,          # some Flask specific configs
-    "CACHE_TYPE": "simple",  # Flask-Caching related configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
     "CACHE_DEFAULT_TIMEOUT": 300
 }
 app = Flask(__name__)
